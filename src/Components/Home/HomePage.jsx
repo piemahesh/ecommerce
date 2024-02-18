2e457428f3a19839f424329d803bc51ccfe2122a
piemahesh <piemahesh@gmail.com>
2024-02-18 23:27:05 +0530

    homepage edit

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -7,6 +7,20 @@ export const HomePage = () => {
     const { products } = useContext(ProductContext)
     console.log(products)
     return (
-        <div>HomePage</div>
+        <section className="flex w-full max-w-screen-2xl flex-wrap items-center justify-evenly mx-auto">
+            {
+                products.map((product, i) => {
+                    return (
+                        <div key={i}>
+                            <div>
+                                <img src={product.img} alt={product.productName} />
+                            </div>
+                            <div>{product.productName}</div>
+                            <div>{product.price}</div>
+                        </div>
+                    )
+                })
+            }
+        </section>
     )
 }
